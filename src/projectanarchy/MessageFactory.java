4b681e96790616da340183c9725484646265fcda
Unknown <jacobpitkin@mail.weber.edu>
2018-07-31 16:18:37 -0600

    Added Messaging Java file

---
+++ src/projectanarchy/MessageFactory.java
@@ -0,0 +1,29 @@
+package projectanarchy;
+
+public class MessageFactory
+{
+	public void sendChat()
+	{
+		
+	}
+	
+	public void sendHitMiss()
+	{
+		
+	}
+	
+	public void sendMove()
+	{
+		
+	}
+	
+	public void sendStart()
+	{
+		
+	}
+	
+	public void sendWin()
+	{
+		
+	}
+}
