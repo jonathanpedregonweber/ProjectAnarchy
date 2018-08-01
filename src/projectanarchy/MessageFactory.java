9f84bec98786b47c0784faa7846ab01447e39883
Clifford Peters <cliffordpeters@mail.weber.edu>
2018-08-01 03:42:49 +0000

    Added Chat message and abstracted the JSON Writer setup. Uses Lamdas instead.

--- src/projectanarchy/MessageFactory.java
+++ src/projectanarchy/MessageFactory.java
@@ -1,6 +1,7 @@
 package projectanarchy;
 
 import java.io.StringWriter;
+import java.util.function.Consumer;
 
 import org.json.JSONWriter;
 
@@ -25,17 +26,38 @@ public class MessageFactory {
 		//
 	}
 	
-	public static String login(String username, String password) {
+	private static String json(Consumer<JSONWriter> consumer) {
 		StringWriter stringWriter = new StringWriter();
 		JSONWriter writer = new JSONWriter(stringWriter);
 		writer.object();
-		writer.key("type").value("login");
-		writer.key("message");
-		writer.object();
-		writer.key("username").value(username);
-		writer.key("password").value(password);
-		writer.endObject();
+		consumer.accept(writer);
 		writer.endObject();
 		return stringWriter.toString();
+	}
+	
+	
+	public static String chat(String message) {
+		return json(writer -> {
+			writer.key("type").value("chat");
+			writer.key("message").value(message);
+		});
+	}
+	
+	public static String chat(String message, String username) {
+		return json(writer -> {
+			writer.key("type").value("chat");
+			writer.key("message").value(message);
+			writer.key("username").value(username);
+		});
+	}
+	public static String login(String username, String password) {
+		return json(writer -> {
+			writer.key("type").value("login");
+			writer.key("message");
+			writer.object();
+			writer.key("username").value(username);
+			writer.key("password").value(password);
+			writer.endObject();
+		});
 	}
 }
