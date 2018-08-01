0b639c2f6f0d22bebb16f589c2a2368d3904921f
Clifford Peters <cliffordpeters@mail.weber.edu>
2018-08-01 03:17:37 +0000

    Added Login function to Message Factory.

--- src/projectanarchy/MessageFactory.java
+++ src/projectanarchy/MessageFactory.java
@@ -1,5 +1,9 @@
 package projectanarchy;
 
+import java.io.StringWriter;
+
+import org.json.JSONWriter;
+
 public class MessageFactory {
 	public static String sendHitMiss(int[] target) {
 		int shipLocation = 0;
@@ -19,5 +23,19 @@ public class MessageFactory {
 	
 	public void sendWin() {
 		//
+	}
+	
+	public static String login(String username, String password) {
+		StringWriter stringWriter = new StringWriter();
+		JSONWriter writer = new JSONWriter(stringWriter);
+		writer.object();
+		writer.key("type").value("login");
+		writer.key("message");
+		writer.object();
+		writer.key("username").value(username);
+		writer.key("password").value(password);
+		writer.endObject();
+		writer.endObject();
+		return stringWriter.toString();
 	}
 }
